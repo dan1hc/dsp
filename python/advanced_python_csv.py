7d6771545ef1726d1a72ce735201cd677dd29df5
Daniel Dube <ec2usr@ip-172-31-5-230.us-west-1.compute.internal>
2016-12-23 09:08:31 +0000

    q5

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,13 @@
-PLACE YOUR CODE HERE
+import pandas as pd
+
+csv_path = '/home/ec2usr/ds/metis/mteisgh/prework/dsp/python/faculty.csv'
+
+faculty_df = pd.read_csv(csv_path)
+
+emails = pd.Series(faculty_df[' email'])
+
+csv_path = '/home/ec2usr/ds/metis/mteisgh/prework/dsp/python/emails.csv'
+
+emails.to_csv(csv_path, index=False)
+
+

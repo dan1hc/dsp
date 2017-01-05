4b8aa8204f3a62e9f57aa10cc5ae17887cb190ec
Daniel Dube <ec2usr@ip-172-31-5-230.us-west-1.compute.internal>
2017-01-05 23:26:10 +0000

    final answers

--- math/matrix_algebra.py
+++ math/matrix_algebra.py
@@ -15,9 +15,87 @@ u = np.array([6,2,-3,5])
 print(u)
 v = np.array([3,5,-1,4])
 print(v)
-w = np.array([1,8,0,5])
-print(w[:, np.newaxis], '\n')
-answers = OrderedDict([('1', np.nan), ('2', A-C.transpose()), ('3', D*3), ('4', B@A), ('5', np.nan), ('6', np.nan), ('7', C@B), ('8', B**4), ('9', A@A.transpose()), ('10', D.transpose()@D)])
+w = np.array([1,8,0,5])[:, np.newaxis]
+print(w, '\n')
+answers = OrderedDict([
+                       ('1.1', A.shape),
+                       ('1.2', B.shape),
+                       ('1.3', C.shape),
+                       ('1.4', D.shape),
+                       ('1.5', (1, 4)),
+                       ('1.6', w.shape),
+                       ('2.1', u+v),
+                       ('2.2', u-v),
+                       ('2.3', 6*u),
+                       ('2.4', np.dot(u,v)),
+                       ('2.5', np.linalg.norm(u)),
+                       ('3.1', np.nan), 
+                       ('3.2', A-C.transpose()), 
+                       ('3.3', D*3), 
+                       ('3.4', B@A), 
+                       ('3.5', np.nan), 
+                       ('3.6', np.nan), 
+                       ('3.7', C@B), 
+                       ('3.8', B**4), 
+                       ('3.9', A@A.transpose()), 
+                       ('3.10', D.transpose()@D)
+                       ])
 
-for a in answers:
-    print(a, answers[a], '\n')
+for a in answers.items():
+    print(a, '\n')
+    
+"""
+ANSWERS (also completed by hand, but I have no scanner)
+('1.1', (2, 3)) 
+
+('1.2', (2, 2)) 
+
+('1.3', (3, 2)) 
+
+('1.4', (2, 3)) 
+
+('1.5', (1, 4)) 
+
+('1.6', (4, 1)) 
+
+('2.1', array([ 9,  7, -4,  9])) 
+
+('2.2', array([ 3, -3, -2,  1])) 
+
+('2.3', array([ 36,  12, -18,  30])) 
+
+('2.4', 51) 
+
+('2.5', 8.6023252670426267) 
+
+('3.1', nan) 
+
+('3.2', array([[-4, -7, -3],
+       [ 3,  6,  4]])) 
+
+('3.3', array([[ 9, -6, -3],
+       [ 3,  6,  9]])) 
+
+('3.4', array([[-1, -5, -1],
+       [ 2,  7,  4]])) 
+
+('3.5', nan) 
+
+('3.6', nan) 
+
+('3.7', array([[ 5, -6],
+       [ 9, -8],
+       [ 6, -6]])) 
+
+('3.8', array([[1, 1],
+       [0, 1]])) 
+
+('3.9', array([[14, 28],
+       [28, 69]])) 
+
+('3.10', array([[10, -4,  0],
+       [-4,  8,  8],
+       [ 0,  8, 10]])) 
+"""
+    
+    
